2a2b0f90c6324464c88d341bfdf3a10dbe3a9545
Ana Beatriz de Souza <abeatriz.dev@gmail.com>
2025-04-17 10:33:25 -0300

    fix: test AthenaOsApplicationTest

--- src/main/java/dev/abeatriz/athena_os/AthenaOsApplication.java
+++ src/main/java/dev/abeatriz/athena_os/AthenaOsApplication.java
@@ -6,16 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class AthenaOsApplication {
     public static void main(String[] args) {
-        if (Files.exists(Paths.get(".env"))) {
+
+        if (!Arrays.asList(args).contains("--spring.profiles.active=test") && Files.exists(Paths.get(".env"))) {
             System.out.println(".env encontrado - carregando variáveis");
             Dotenv.configure().load().entries().forEach(e ->
                     System.setProperty(e.getKey(), e.getValue()));
         }
 
+        System.out.println("Não carregou .env");
         SpringApplication.run(AthenaOsApplication.class, args);
     }
 }
